259f8336bd192af0a76acff57d42d89cf54f4985
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-14 10:15:35 +0530

    auto-update test

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ sudo bash /etc/GitCatcher/uninstall.sh
 cd /tmp
 git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
 cd /tmp/GitCatcher
+git config --global --add safe.directory /etc/GitCatcher
 git checkout beta
 sudo bash init.sh $emailID
