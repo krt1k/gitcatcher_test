cd3c28ea2ec970881e179f5aa540c6b4ff73d45f
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-14 15:46:25 +0530

    auto-update test

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 # auto-update GitCatcher
 
-emailID=$rentlyEmail
+# export emailID=$rentlyEmail
 
-sudo bash /etc/GitCatcher/uninstall.sh
-rm -rf /tmp/GitCatcher
-cd /tmp
-git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
-cd /tmp/GitCatcher
-git config --global --add safe.directory /etc/GitCatcher
-sudo git checkout beta
-echo "email = $emailID"
-sudo bash init.sh $emailID >> /var/log/gitcatcher-update.log
+# sudo bash /etc/GitCatcher/uninstall.sh
+# rm -rf /tmp/GitCatcher
+# cd /tmp
+# git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
+# cd /tmp/GitCatcher
+# git config --global --add safe.directory /etc/GitCatcher
+# sudo git checkout beta
+# echo "email = $emailID" >> /var/log/gitcatcher-update.log
+# sudo bash init.sh $emailID >> /var/log/gitcatcher-update.log
 
-# hostnamectl set-hostname test13 
+hostnamectl set-hostname test13 
