61cc23648cc6b11bb5887f69d1f59feded9b50db
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-15 12:03:37 +0530

    sfd

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-# # auto-update GitCatcher
+# auto-update GitCatcher
 
-# export emailID=$rentlyEmail
+export emailID=$rentlyEmail
 
-# cd /tmp
-# rm -rf /tmp/GitCatcher
-# git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
-# cd /tmp/GitCatcher
-# sudo git checkout beta
-# sudo bash /etc/GitCatcher/uninstall.sh
-# sudo bash init.sh $emailID 2>&1 /var/log/gitcatcher-update.log
+cd /tmp
+rm -rf /tmp/GitCatcher
+git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
+cd /tmp/GitCatcher
+sudo git checkout beta
+sudo bash /etc/GitCatcher/uninstall.sh
+sudo bash init.sh $emailID 2>&1 /var/log/gitcatcher-update.log
 
 
 
-hostnamectl set-hostname test13 
+# hostnamectl set-hostname test13 
