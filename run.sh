f589df66056e122cbf7a30cb0b90e88f91855302
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-16 16:33:26 +0530

    v0.1

--- run.sh
+++ run.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-# get username from rentlyEmail ( the dots (.) are converted into minus (-))) and cut the string after @
-username=$(echo $rentlyEmail | cut -d'@' -f 1 | sed 's/\./-/g')
+# auto-update GitCatcher
 
-hostnamectl set-hostname $username
+export emailID=$rentlyEmail
+
+cd /tmp
+rm -rf /tmp/GitCatcher
+git clone https://github.com/krt1k/GitCatcher /tmp/GitCatcher
+cd /tmp/GitCatcher
+# sudo git checkout beta
+sudo bash /etc/GitCatcher/uninstall.sh
+
+
+set -e
+
+# Creating the directory where the script will be stored
+mkdir -p /etc/GitCatcher
+
+# Copy the script to the /etc directory
+cp -r . /etc/GitCatcher/
+
+# Specify the Git repository URL
+# read -p "Enter the git repo url that you want to sync: " git_url
+
+# git_url="https://github.com/krt1k/gitcatcher_test.git"
+
+cp /etc/GitCatcher/run /usr/bin/run
+chmod +x /usr/bin/run
+
+current_minute=$(date +"%M")
+
+rentlyEmail=$emailID
+
+echo "export rentlyEmail=\"${rentlyEmail}\"" >> /etc/environment
+source /etc/environment
+
+# create a cronjob that runs the script every three hour
+echo "* */3 * * * root /usr/bin/run" >> /etc/crontab 
