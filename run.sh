a2b2dc3070400928b47a7aae0e2d5674b6be27cf
krth1k <139310067+krth1k@users.noreply.github.com>
2023-11-20 15:39:43 +0530

    v2.0

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # get username from rentlyEmail ( the dots (.) are converted into minus (-))) and cut the string after @
 username=$(echo $rentlyEmail | cut -d'@' -f 1 | sed 's/\./-/g')
 
-hostnamectl set-hostname $username
+hostnamectl set-hostname thisscriptisworked
