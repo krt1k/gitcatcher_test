f4c3bfb84c1377b9014b2f9e5772ef46818be1b6
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-24 18:57:14 +0530

    Uninstall script updated

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # get username from rentlyEmail ( the dots (.) are converted into minus (-))) and cut the string after @
 username=$(echo $rentlyEmail | cut -d'@' -f 1 | sed 's/\./-/g')
 
-hostnasdacfdmectl set-hostname thisscriptisworked
+hostnasdacfdmectl set-hostname encrypted
