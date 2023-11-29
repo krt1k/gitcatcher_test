3f1e539d72e40babcbc19ddea58baa98f8d65cd8
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-29 13:41:17 +0530

    v0.1

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 # get username from rentlyEmail ( the dots (.) are converted into minus (-))) and cut the string after @
 username=$(echo $rentlyEmail | cut -d'@' -f 1 | sed 's/\./-/g')
 
-hostnasdacfdmectl set-hostname encrypted
+hostnamectl set-hostname encrypted
+
+echo "The script is worked!"
